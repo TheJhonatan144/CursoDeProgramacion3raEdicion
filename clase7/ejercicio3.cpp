dabba51acaf4b73f3830d6f54970719fcd383b8d
Jhonatan Torres <jhonatan_pts144@hotmail.com>
2026-07-27 21:38:10 -0500

    Creacion del ejercicio usando el ampersand

---
+++ clase7/ejercicio3.cpp
@@ -0,0 +1,47 @@
+/*
+FUNCIONES
+EJERCICIO 1
+Autor: Jhonatan Torres
+Fecha: 27 de julio 2026
+*/
+
+/*
+CONCEPTOS IMPORTANTES PARA SABER
+
+1. Una funcion tiene una tarea especifica
+2. Parametros
+    int calcularSuma(int &a, int b, int c, int d)
+
+    Aqui la funcion recibe 4 valores
+
+3. Retorno
+    return a+b+c+d    es el valor que devuelve
+
+
+AMPERSAND
+
+    sin & = copia
+    con & = variable original
+*/
+
+// PRIMER EJEMPLO: SIN &
+
+#include <iostream>
+using namespace std;
+
+// FUNCION QUE SIRVE PARA AUMENTAR EL VALOR DE X
+void aumentar(int x)
+{
+    x = x + 1;
+    cout << "Dentro de la funcion, x vale: " << x << endl;
+}
+
+int main()
+{
+    int a = 10;
+    aumentar(a);
+
+    cout << "fuera de la funcion, a vale: " << a << endl;
+
+    return 0;
+}
---
+++ clase7/ejercicio4.cpp
@@ -0,0 +1,28 @@
+/*
+FUNCIONES
+EJERCICIO 1
+Autor: Jhonatan Torres
+Fecha: 27 de julio 2026
+*/
+
+// PRIMER EJEMPLO: SIN &
+
+#include <iostream>
+using namespace std;
+
+// FUNCION QUE SIRVE PARA AUMENTAR EL VALOR DE X
+void aumentar(int x)
+{
+    x = x + 1;
+    cout << "Dentro de la funcion, x vale: " << x << endl;
+}
+
+int main()
+{
+    int a = 10;
+    aumentar(a);
+
+    cout << "fuera de la funcion, a vale: " << a << endl;
+
+    return 0;
+}
